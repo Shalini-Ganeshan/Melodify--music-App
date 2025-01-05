194fc9cfb06c9fa9eaef1af05c7becbf6d974826
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-01-05 10:54:52 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -60,15 +60,15 @@ function Card({ element }) {
         </div>
 
         <div className="p-4 flex-grow">
-          <h5 className="text-sm h-4 overflow-hidden font-poppins mb-1">{element.name}</h5>
+          <h5 className="text-sm h-4 overflow-hidden font-poppins font-bold mb-1">{element.name}</h5>
           <div className="flex flex-row">
-            <p className="text-black font-bold text-sm mr-2">Artist:</p>
+            <p className="text-black  text-sm mr-2">Artist:</p>
             <p className="text-xs text-gray-600 overflow-hidden max-w-xs">
               {element.album.artists[0].name}
             </p>    
           </div>  
           <div className="flex flex-row">  
-            <p className="text-black font-bold text-sm mr-2 ">Release:</p> 
+            <p className="text-black  text-sm mr-2 ">Release:</p> 
             <p className="text-xs text-gray-600"> {element.album.release_date}</p>
           </div>  
           <audio src={element.preview_url} controls className="w-full border-2 border-black rounded-full "></audio>
